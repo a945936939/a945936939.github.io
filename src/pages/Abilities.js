285698a7b7b3bcf5e1f6a3e71bc537993d733f9c
zguo0040 <zguo0040@student.monash.edu>
2023-08-17 16:40:47 +1000

    added some fade in animation

--- src/pages/Abilities.js
+++ src/pages/Abilities.js
@@ -4,13 +4,13 @@ export default function Abilities() {
   return (
     <div>
       <div className="text-center">
-        <h3 className="text-3xl py-1">Services I offer </h3>
-        <p className="text-md py-2 leading-8 text-gray-800">
+        <h3 className="py-1 text-3xl ">Services I offer </h3>
+        <p className="py-2 leading-8 text-gray-800 text-md">
           After graduating from Monash University, I have done several projects
           about <span className="text-teal-500">Web development </span>and some
           <span className="text-teal-500"> content design </span>
         </p>
-        <p className="text-md py-2 leading-8 text-gray-800">
+        <p className="py-2 leading-8 text-gray-800 text-md">
           And now i am looking for a job about web development.
         </p>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
-import { Inter } from "next/font/google";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { motion, useScroll } from "framer-motion";
 import { AiFillLinkedin, AiFillGithub, AiFillPlayCircle } from "react-icons/ai";
 
 import Image from "next/image";
@@ -30,21 +29,21 @@ export default function Home() {
   const [darkMode, setIsDark] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
-      <main className=" bg-cover bg-fixed bg-[url('/campus.png')]">
+      <main className=" bg-[url('/campus.png')] bg-cover bg-fixed">
         <div className="backdrop-blur-sm">
-          <nav className="py-10 pb-12 flex justify-between text-white bg-gradient-to-br from-slate-950 to-teal-950 px-10 md:px-20 lg:px-40 ">
-            <h1 className="text-2xl font-burtons ">TRUMAN PORTFOLIO</h1>
+          <nav className="flex justify-between bg-gradient-to-br from-slate-950 to-teal-950 px-10 py-10 pb-12 text-white md:px-20 lg:px-40 ">
+            <h1 className="font-burtons text-2xl ">TRUMAN PORTFOLIO</h1>
 
             <ul className="flex items-center">
               {/* <li>
               <BsFillMoonStarsFill
                 onClick={() => setIsDark(!darkMode)}
-                className="cursor-pointer text-2xl"
+                className="text-2xl cursor-pointer"
               />
             </li> */}
-              <li className="  transition duration-500 ease-in-out  hover:scale-125">
+              <li className="transition duration-500 ease-in-out hover:scale-125">
                 <a
-                  className="transition duration-500 ease-in-out bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8   hover:text-teal-900 hover:bg-teal-800"
+                  className="ml-8 rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 text-white transition duration-500 ease-in-out hover:bg-teal-800 hover:text-teal-900"
                   href="/public/Resume.pdf"
                   download
                 >
@@ -53,56 +52,71 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="    ">
-            <section className="min-h-screen   bg-teal-50  p-3 ">
-              <div className="text-yellow-950 text-center p-10 md:p-20">
-                <h2 className="text-5xl py-2 text-teal-700 font-medium md:text-6xl">
+          <div className="">
+            <section className="min-h-screen bg-teal-50 p-3">
+              <div className="p-10 text-center text-yellow-950 md:p-20">
+                <h2 className="py-2 text-5xl font-medium text-teal-700 md:text-6xl">
                   TRUMAN GUO
                 </h2>
-                <h3 className="text-2xl py-2 md:text-3xl">
+                <h3 className="py-2 text-2xl md:text-3xl">
                   Software Engineer.
                 </h3>
-                <p className="text-md py-5 leading-8 text-gray-800 md-text-xl max-w-lg mx-auto">
+                <p className="text-md md-text-xl mx-auto max-w-lg py-5 leading-8 text-gray-800 ">
                   Being Passionate,being innovative,I am a graduate software
                   engineer who loves programming in Web and Games.
                 </p>
               </div>
-              <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+              <div className="flex justify-center gap-16 py-3 text-5xl text-gray-600">
                 <AiFillLinkedin
-                  className="hover:cursor-pointer  transition duration-500 ease-in-out  hover:scale-125"
+                  className="transition duration-500 ease-in-out hover:scale-125 hover:cursor-pointer"
                   onClick={() =>
                     window.open(
-                      "https://www.linkedin.com/in/truman-guo-946a80204/"
+                      "https://www.linkedin.com/in/truman-guo-946a80204/",
                     )
                   }
                 />
 
                 <AiFillGithub
-                  className="hover:cursor-pointer  transition duration-500 ease-in-out  hover:scale-125"
+                  className="transition duration-500 ease-in-out hover:scale-125 hover:cursor-pointer"
                   onClick={() => window.open("https://github.com/a945936939")}
                 />
               </div>
-              <div className="relative bg-gradient-to-b from-teal-500 rounded-full  mt-1 sm:mt-5 md:mt-10 mx-auto w-32 h-32 overflow-hidden md:h-96 md:w-96 sm:w-48 sm:h-48">
-                <Image src={deved} layout="fill" objectFit="cover" />
+              <div className="relative mx-auto my-10 mt-1 h-32 w-32 overflow-hidden rounded-full bg-gradient-to-b from-teal-500 sm:mt-5 sm:h-48 sm:w-48 md:mt-10 md:h-96 md:w-96">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  transition={{ duration: 1.5 }}
+                  whileInView={{ opacity: 1 }}
+                >
+                  <Image src={deved} layout="fill" objectFit="cover" />
+                </motion.div>
               </div>
             </section>
-            <div className="m-24 lg:m-96 md:m-48 ">
-              <h3 className="text-xl md:text-2xl lg:text-6xl py-1 text-white text-center">
-                PASSIONATE INNOVATIVE
-              </h3>
+
+            <div className="m-24 md:m-48 lg:m-96 ">
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1.5 }}
+                whileInView={{ opacity: 1 }}
+              >
+                <h3 className="py-1 text-center text-xl text-white md:text-2xl lg:text-6xl">
+                  PASSIONATE INNOVATIVE
+                </h3>
+              </motion.div>
             </div>
-            <section className="pb-10 bg-white  py-3 px-10 md:px-20 lg:px-40">
-              <Abilities />
-              <div className="flex flex-col lg:flex-row md:gap-5 ">
-                <Service content={content1} title={title1} src={design} />
-                <Service content={content2} title={title2} src={code} />
-                <Service content={content3} title={title3} src={consulting} />
-              </div>
-            </section>
-            <section className=" shadow-lg text-center bg-teal-50   py-3 px-10 md:px-20 lg:px-40">
+            <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+              <section className="bg-white px-10 py-3 pb-10 md:px-20 lg:px-40">
+                <Abilities />
+                <div className="my-10 flex flex-col md:gap-5 lg:flex-row ">
+                  <Service content={content1} title={title1} src={design} />
+                  <Service content={content2} title={title2} src={code} />
+                  <Service content={content3} title={title3} src={consulting} />
+                </div>
+              </section>
+            </motion.div>
+            <section className="bg-teal-50 px-10 py-10  text-center shadow-lg md:px-20 lg:px-40">
               <div>
-                <h3 className="text-3xl py-1">Work I have done</h3>
-                <h3 className="text-lg font-medium pt-8 pb-2"> Designs</h3>
+                <h3 className="py-1 text-3xl">Work I have done</h3>
+                <h3 className="pb-2 pt-8 text-lg font-medium"> Designs</h3>
                 <p className="text-md py-2 leading-8 text-gray-800">
                   Design is the key to a great user experience. I can help the
                   team to design aesthetic products in
@@ -111,18 +125,25 @@ export default function Home() {
                   <span className="text-teal-500"> Games</span>
                 </p>
               </div>
+
               <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                <div className="basis-1/3 flex-1">
-                  <Image
-                    className="rounded-lg object-cover"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={game1}
-                  />
-                </div>
-                <div className="basis-1/3 flex-1">
-                  <h3 className="text-2xl font-medium pt-8 pb-2"> Jump Man</h3>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  transition={{ duration: 1.5 }}
+                  whileInView={{ opacity: 1 }}
+                >
+                  <div className="flex-1 basis-1/3">
+                    <Image
+                      className="rounded-lg object-cover"
+                      width={"100%"}
+                      height={"100%"}
+                      layout="responsive"
+                      src={game1}
+                    />
+                  </div>
+                </motion.div>
+                <div className="flex-1 basis-1/3">
+                  <h3 className="pb-2 pt-8 text-2xl font-medium"> Jump Man</h3>
                   <p className="text-md py-2 leading-8 text-gray-800">
                     Press the <span className="text-teal-500">spacebar</span> to
                     jump to avoid the obstacles.
@@ -134,7 +155,7 @@ export default function Home() {
                   </p>
                   <div className="flex justify-center text-center">
                     <AiFillPlayCircle
-                      className="w-36 h-36 cursor-pointer"
+                      className="h-36 w-36 cursor-pointer"
                       onClick={() => {
                         window.open("https://truman1998.itch.io/jump-man");
                       }}
@@ -146,103 +167,116 @@ export default function Home() {
                   <a
                     href="https://github.com/a945936939/Prototype3
 "
-                    className="text-md py-2 leading-8 text-gray-800 cursor-pointer underline hover:text-red-500"
+                    className="text-md cursor-pointer py-2 leading-8 text-gray-800 underline hover:text-red-500"
                   >
                     https://github.com/a945936939/Prototype3
                   </a>
                 </div>
               </div>
-              <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                <div className="basis-1/3 flex-1">
-                  <h3 className="text-2xl font-medium pt-8 pb-2">
-                    {" "}
-                    Fitness app
-                  </h3>
-                  <p className="text-md py-2 leading-8 text-gray-800">
-                    Built a fitness app on{" "}
-                    <span className="text-teal-500">Android Platform</span>{" "}
-                    (demonstrated on emulators)
-                  </p>
 
-                  <p className="text-md py-2 leading-8 text-gray-800">
-                    Github Link(private visibility , will change to public if
-                    wants to have a look):
-                  </p>
-                  <a
-                    href="https://github.com/a945936939/Prototype3
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1.5 }}
+                whileInView={{ opacity: 1 }}
+              >
+                <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+                  <div className="flex-1 basis-1/3">
+                    <h3 className="pb-2 pt-8 text-2xl font-medium">
+                      {" "}
+                      Fitness app
+                    </h3>
+                    <p className="text-md py-2 leading-8 text-gray-800">
+                      Built a fitness app on{" "}
+                      <span className="text-teal-500">Android Platform</span>{" "}
+                      (demonstrated on emulators)
+                    </p>
+
+                    <p className="text-md py-2 leading-8 text-gray-800">
+                      Github Link(private visibility , will change to public if
+                      wants to have a look):
+                    </p>
+                    <a
+                      href="https://github.com/a945936939/Prototype3
 "
-                    className="text-md py-2 leading-8 text-gray-800 cursor-pointer underline hover:text-red-500"
-                  >
-                    https://github.com/a945936939/FIT5046
-                  </a>
-                </div>
-                <div className="basis-1/7 flex-1">
-                  <Image
-                    className="rounded-lg object-cover"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={android1}
-                  />
-                </div>
-                <div className="basis-1/7 flex-1">
-                  <Image
-                    className="rounded-lg object-cover"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={android2}
-                  />
-                </div>
-                <div className="basis-1/7 flex-1">
-                  <Image
-                    className="rounded-lg object-cover"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={android3}
-                  />
-                </div>
-              </div>
+                      className="text-md cursor-pointer py-2 leading-8 text-gray-800 underline hover:text-red-500"
+                    >
+                      https://github.com/a945936939/FIT5046
+                    </a>
+                  </div>
 
-              <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                <div className="basis-1/3 flex-1">
-                  <Image
-                    className="rounded-lg object-cover"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={futurecity}
-                  />
+                  <div className="basis-1/7 flex-1">
+                    <Image
+                      className="rounded-lg object-cover"
+                      width={"100%"}
+                      height={"100%"}
+                      layout="responsive"
+                      src={android1}
+                    />
+                  </div>
+                  <div className="basis-1/7 flex-1">
+                    <Image
+                      className="rounded-lg object-cover"
+                      width={"100%"}
+                      height={"100%"}
+                      layout="responsive"
+                      src={android2}
+                    />
+                  </div>
+                  <div className="basis-1/7 flex-1">
+                    <Image
+                      className="rounded-lg object-cover"
+                      width={"100%"}
+                      height={"100%"}
+                      layout="responsive"
+                      src={android3}
+                    />
+                  </div>
                 </div>
-                <div className="basis-1/3 flex-1">
-                  <h3 className="text-2xl font-medium pt-8 pb-2">
-                    {" "}
-                    Emission Tracker
-                  </h3>
-                  <p className="text-md py-2 leading-8 text-gray-800">
-                    Using{" "}
-                    <span className="text-teal-500">
-                      PHP, CSS, Bootstrap, Cloud Deploy
-                    </span>{" "}
-                    and built a website which can track your carbon footprint
-                    based on your destination, and will show the result on our
-                    modified google map.
-                  </p>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1.5 }}
+                whileInView={{ opacity: 1 }}
+              >
+                <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+                  <div className="flex-1 basis-1/3">
+                    <Image
+                      className="rounded-lg object-cover"
+                      width={"100%"}
+                      height={"100%"}
+                      layout="responsive"
+                      src={futurecity}
+                    />
+                  </div>
+                  <div className="flex-1 basis-1/3">
+                    <h3 className="pb-2 pt-8 text-2xl font-medium">
+                      {" "}
+                      Emission Tracker
+                    </h3>
+                    <p className="text-md py-2 leading-8 text-gray-800">
+                      Using{" "}
+                      <span className="text-teal-500">
+                        PHP, CSS, Bootstrap, Cloud Deploy
+                      </span>{" "}
+                      and built a website which can track your carbon footprint
+                      based on your destination, and will show the result on our
+                      modified google map.
+                    </p>
 
-                  <p className="text-md py-2 leading-8 text-gray-800">
-                    Github Link(private visibility , will change to public if
-                    wants to have a look):
-                  </p>
-                  <a
-                    href="https://github.com/a945936939/Prototype3
+                    <p className="text-md py-2 leading-8 text-gray-800">
+                      Github Link(private visibility , will change to public if
+                      wants to have a look):
+                    </p>
+                    <a
+                      href="https://github.com/a945936939/Prototype3
 "
-                    className="text-md py-2 leading-8 text-gray-800 cursor-pointer underline hover:text-red-500"
-                  >
-                    https://github.com/a945936939/FutureCity
-                  </a>
+                      className="text-md cursor-pointer py-2 leading-8 text-gray-800 underline hover:text-red-500"
+                    >
+                      https://github.com/a945936939/FutureCity
+                    </a>
+                  </div>
                 </div>
-              </div>
+              </motion.div>
             </section>
           </div>
         </div>
