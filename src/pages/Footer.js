d2e3fd7d86fc8985ee36eaac5545f036d15e8003
a945936939 <945936939@qq.com>
2023-08-19 14:12:08 +1000

    Update Footer.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,15 +1,13 @@
-import React from 'react'
-
-export default function 
-() {
+import React from "react";
+export default function () {
   return (
-    <div className='p-8 bg-black text-gray-300 text-center font-thin font-burtons'>
-        <h3 className='text-lg mb-3'>Truman Developer's copyright</h3>
-        <div className='flex justify-center '>
-        <p className='text-xs mx-3'>Truman 2023. All rights reserved.  </p>
-        <p className='text-xs mx-3'>trumangzp@gmail.com  </p>
-        <p className='text-xs mx-3'>0431636567  </p>
-        </div>
+    <div className="bg-black p-8 text-center font-burtons font-thin text-gray-300">
+      <h3 className="mb-3 text-lg">Truman Developer's copyright</h3>
+      <div className="flex justify-center ">
+        <p className="mx-3 text-xs">Truman 2023. All rights reserved. </p>
+        <p className="mx-3 text-xs">trumangzp@gmail.com </p>
+        <p className="mx-3 text-xs">0431636567 </p>
+      </div>
     </div>
-  )
+  );
 }
