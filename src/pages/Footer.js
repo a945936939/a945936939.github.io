cb764067df109cdb79c75e6e5869aeb05c0855c9
a945936939 <945936939@qq.com>
2023-08-19 14:09:52 +1000

    Update Footer.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,7 +4,7 @@ export default function
 () {
   return (
     <div className='p-8 bg-black text-gray-300 text-center font-thin font-burtons'>
-        <h3 className='text-lg'>Truman Developer's copyright</h3>
+        <h3 className='text-lg mb-3'>Truman Developer's copyright</h3>
         <div className='flex justify-center '>
         <p className='text-xs mx-3'>Truman 2023. All rights reserved.  </p>
         <p className='text-xs mx-3'>trumangzp@gmail.com  </p>
