127e3a3fbd4684ad39b4e4eee7842990d85daced
Truman Guo <31941075+a945936939@users.noreply.github.com>
2025-01-03 18:11:47 +1100

    Update Personalintro.js

--- src/pages/Personalintro.js
+++ src/pages/Personalintro.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import BIRDS from "vanta/dist/vanta.birds.min";
+// import BIRDS from "vanta/dist/vanta.birds.min";
 import Image from "next/image";
 import { AiFillLinkedin, AiFillGithub, AiFillPlayCircle } from "react-icons/ai";
 import Typewriter from "typewriter-effect";
@@ -12,33 +12,41 @@ import tailwindPic from "public/icons/icons8-tailwind-css-48.webp";
 import { motion } from "framer-motion";
 
 export default function Personalintro() {
-  const [vantaEffect, setVantaEffect] = useState(null);
+  // const [vantaEffect, setVantaEffect] = useState(null);
   const myRef = useRef(null);
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        BIRDS({
-          el: myRef.current,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 1.0,
-          backgroundColor: 0x424a,
-          quantity: 3.0,
-        }),
-      );
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       BIRDS({
+  //         el: myRef.current,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: false,
+  //         minHeight: 200.0,
+  //         minWidth: 200.0,
+  //         scale: 1.0,
+  //         scaleMobile: 1.0,
+  //         backgroundColor: 0x424a,
+  //         quantity: 3.0,
+  //       }),
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, [vantaEffect]);
   return (
     <>
       <div ref={myRef}>
         <div className="pb-20">
+          <div className="flex justify-center gap-10 py-10">
+            <a
+              className="py-2 text-center text-2xl font-black text-red-500 underline md:text-2xl"
+              href="https://trumanportfolio.vercel.app/"
+            >
+              THIS PORTFOLIO IS OUTDATED, CLICK HERE TO SEE THE NEW ONE
+            </a>
+          </div>
           <div className="bg p-10 text-center text-white md:p-20">
             <h2 className="py-2 text-5xl font-black text-white md:text-6xl">
               TRUMAN GUO
