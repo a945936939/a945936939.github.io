e631824c773b541058e6ef3948f54a9e1e2f5825
AiimsGuo <gzhipeng@aiims.com.au>
2025-01-14 16:14:43 +1100

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/a945936939.github.io",
+  assetPrefix: "/a945936939.github.io",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
